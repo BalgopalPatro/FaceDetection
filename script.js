40421ce2c43bee1ac5f4088cec5bfcd67e2a8ae3
N Balgopal Patro <46865367+BalgopalPatro@users.noreply.github.com>
2019-11-27 10:17:39 +0530

    Edited Script

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const video = document.getElementById('video')
 
 
 Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+    faceapi.nets.tinyFaceDetector.loadFromUri('https://github.com/BalgopalPatro/FaceDetection/tree/master/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('https://github.com/BalgopalPatro/FaceDetection/tree/master/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('https://github.com/BalgopalPatro/FaceDetection/tree/master/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('https://github.com/BalgopalPatro/FaceDetection/tree/master/models')
   ]).then(startVideo)
 
 function startVideo(){
